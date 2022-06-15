b94041f5aecf24acdcf009d1db473c9005719b00
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-06-15 00:58:34 -0300

    Refactory: Ajusted Create Ebit Method

--- react/components/SeloEbit/index.tsx
+++ react/components/SeloEbit/index.tsx
@@ -1,58 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { OrderGroupContext } from "vtex.order-placed";
-
-import { ProductProps } from "./seloEbit.types";
+import getOrderSpecifications from "../../utils/getOrderSpecifications";
 
 import styles from "./styles.module.css";
 
 const SeloEbit: FC = () => {
   const { useOrderGroup } = OrderGroupContext;
-
   const orderGroup = useOrderGroup();
-
   const order = orderGroup?.orders[0];
-  const { value, items, clientProfileData, paymentData, deliveryParcels } =
-    orderGroup?.orders[0];
 
-  const { address, seller } = deliveryParcels[0];
+  const storeId = 76732;
 
-  const { transactionId, payments } = paymentData.transactions[0];
-  const { firstName } = clientProfileData;
-  const { postalCode: zipCode, price: deliveryTax } = address;
-  const { installments: parcels } = payments[0];
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.id = "getSelo";
+    script.src = `https://imgs.ebit.com.br/ebitBR/selo-ebit/js/getSelo.js?${storeId}`;
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
-  const resumeItems = items.map(
-    ({ name, skuName, price, quantity }: ProductProps) => ({
-      name,
-      skuName,
-      price,
-      quantity,
-    })
-  );
-
-  // const deliveryTime = Number(shippingEstimate.replace(/\D/g, ''))
-
-  console.log({
-    firstName,
-    transactionId,
-    parcels,
-    zipCode,
-    deliveryTax,
-    seller,
-    value,
-    resumeItems,
-  });
-
-  console.log(order);
+  const specificationOrderData = getOrderSpecifications(order, 76732);
 
   return (
     <div className={styles.bannerEbitContainer}>
-      <param
-        id="ebitParam"
-        value={`storeId=76732&transactionId=${transactionId}&platform=1&zipCode=${zipCode}&parcels=${parcels}&deliveryTax=${deliveryTax}&deliveryTime=${1} &totalSpent=${value.toFixed(
-          2
-        )}&value=${value}&quantity=1 &productName=productName&paymentType=paymentType&sku=sku &productCondition=0&deliveryType=deliveryType&mktSaleId= mktSaleId='LojaBuettner'&l5=''&man=''&brd=''&brds=''`}
-      />
+      <param id="ebitParam" value={specificationOrderData} />
 
       <a id="bannerEbit"></a>
     </div>
